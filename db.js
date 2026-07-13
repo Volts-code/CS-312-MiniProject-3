0f8928b54761e871e98976f533a166ed78838461
Volts-code <jm6259@nau.edu>
2026-07-12 18:24:27 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
-import { neon } from "@neondatabase/serverless";
-
-export async function getData() {
-    const sql = neon(process.env.DATABASE_URL);
-    const data = await sql`SELECT * FROM posts;`;
-    return data;
-}
+const { Pool } = require("pg");
+const pool = new Pool({
+    connectionString:"postgresql://neondb_owner:npg_auoIPV31efWT@ep-odd-hat-atwtlsa7-pooler.c-9.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
+    ssl: { rejectUnauthorized: true },
+    port: 5432
+});
