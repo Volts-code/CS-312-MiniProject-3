ee5d30eef5169cca5488bd56225d2f04a2b2673e
Volts-code <jm6259@nau.edu>
2026-07-12 18:11:56 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-    connectionString:"postgresql://neondb_owner:npg_auoIPV31efWT@ep-odd-hat-atwtlsa7-pooler.c-9.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
+    connectionString:"postgresql://neondb_owner:npg_1h8zWcvmrZQH@ep-curly-resonance-atrwrj3a-pooler.c-9.us-east-1.aws.neon.tech/neondb?channel_binding=require&sslmode=require",
     ssl: { rejectUnauthorized: true },
     port: 5432
 });
